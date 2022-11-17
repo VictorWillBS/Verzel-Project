16ce6d88bcb1ce7ba916f7a7ecca48d05b1abd44
Victor Barros <coffiekingb@gmail.com>
2022-11-17 02:05:58 -0300

    :sparkles: Feat: Create User navbarr button

---
+++ src/Components/MenuButtons/UserAvatar.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { HiOutlineUserCircle } from 'react-icons/hi';
+import { IconContext } from 'react-icons';
+export function UserButtom() {
+  const [userData, setUserData] = useState('Victor');
+  return (
+    <UserSection>
+      <IconContext.Provider
+        value={{
+          size: '30px',
+          style: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        }}
+      >
+        <HiOutlineUserCircle />
+      </IconContext.Provider>
+      <h5>{userData}</h5>
+    </UserSection>
+  );
+}
+const UserSection = styled.section`
+  max-width: 200px;
+  width: 100%;
+  height: 85px;
+  margin-right: 20px;
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h5 {
+    font-size: 18px;
+    font-weight: 600;
+
+    :active {
+      color: #1e88e5;
+    }
+  }
+  :hover {
+    cursor: pointer;
+  }
+  @media (min-width: 1000px) and (max-width: 1200px) {
+    h5 {
+      display: none;
+    }
+  }
+  @media (min-width: 0px) and (max-width: 900px) {
+    display: none;
+  }
+`;
