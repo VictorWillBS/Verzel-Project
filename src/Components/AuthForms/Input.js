da5ce952d70013f8cebfbc562fc8280bd0d4a8bd
Victor Barros <coffiekingb@gmail.com>
2022-11-17 22:08:32 -0300

    :sparkles: Feat: Input React Control

--- src/Components/AuthForms/Input.js
+++ src/Components/AuthForms/Input.js
@@ -1,29 +1,72 @@
 import TextField from '@mui/material/TextField';
 
-export default function Input({ isSignUp }) {
-  function returnInput() {
-    if (isSignUp === true) {
-      return (
-        <>
-          {' '}
-          <TextField id="signUp-Input" label="Nome" variant="outlined" />
-          <TextField id="signIn-Input" label="E-mail" variant="outlined" />
-          <TextField id="signIn-Input" label="Senha" variant="outlined" />
-          <TextField
-            id="signUp-Input"
-            label="Confirme a Senha"
-            variant="outlined"
-          />
-        </>
-      );
-    } else {
-      return (
-        <>
-          <TextField id="signIn-Input" label="E-mail" variant="outlined" />
-          <TextField id="signIn-Input" label="Senha" variant="outlined" />
-        </>
-      );
-    }
+function returnSignInInput(insertSignData, signData) {
+  return (
+    <>
+      <TextField
+        id="signIn-Input"
+        label="E-mail"
+        name="email"
+        type="email"
+        value={signData.email}
+        variant="outlined"
+        onChange={(e) => {
+          insertSignData(e);
+        }}
+      />
+      <TextField
+        id="signIn-Input password"
+        label="Senha"
+        variant="outlined"
+        name="password"
+        value={signData.password}
+        type="password"
+        onChange={(e) => {
+          insertSignData(e);
+        }}
+      />
+    </>
+  );
+}
+
+function returnSignUpInput(insertSignData, signData) {
+  return (
+    <>
+      <TextField
+        id="signUp-Input"
+        label="Nome"
+        name="name"
+        type="text"
+        value={signData.name}
+        variant="outlined"
+        onChange={(e) => {
+          insertSignData(e);
+        }}
+      />
+      {returnSignInInput(insertSignData, signData)}
+      <TextField
+        id="signUp-Input"
+        label="Confirme a Senha"
+        variant="outlined"
+        type="password"
+        value={signData.confirmPassword}
+        name="confirmPassword"
+        onChange={(e) => {
+          insertSignData(e);
+        }}
+      />
+    </>
+  );
+}
+
+export default function Input({ isSignUp, signData, setSignData }) {
+  function insertSignData(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    setSignData({ ...signData, [name]: value });
   }
-  return returnInput();
+
+  return isSignUp === true
+    ? returnSignUpInput(insertSignData, signData)
+    : returnSignInInput(insertSignData, signData);
 }
